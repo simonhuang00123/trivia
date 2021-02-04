b5fb0803f5f80c3981ffbc41534574d636428820
simonhuang00123 <45804450+simonhuang00123@users.noreply.github.com>
2021-02-03 17:57:37 -0800

    a

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,33 @@ import "../css/App.css";
 import data from "../sample_data.json";
 
 function App() {
-  return <div className="app">Trivia!</div>;
+  var currentQuestion = 0;
+  return <div>
+  <div className="app">Trivia!</div>
+    <div className="App"> 
+        <Question name={data[currentQuestion].question.text} />
+    </div>
+    <div>
+    <Answer name = {data[currentQuestion].question.choices}/>
+    </div>
+    <div >
+    <NextQuestion />
+      </div> 
+   </div>
+}
+function Answer(props){
+return <div> {props.name}
+  </div>
+}
+function Question(props) {
+  return <div>
+     <div> {props.name}</div>
+  <div>
+  </div>
+  </div>
+}
+function NextQuestion(props) {
+  return <button> Next Question{props.name}</button>;
 }
 
 export default App;
